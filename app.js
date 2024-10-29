fd4237899fc3853f4bcf2452b276a9d8dac138a7
julian E. Aquino <julian.aquino@cencosud.com.ar>
2024-10-29 20:57:37 -0300

    subo correciones

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ const restar = require('./modules/restar');
 
 const multiplicar = require('./modules/multiplicar');
 const dividir = require('./modules/dividir');
+
+const prompt = require('prompt-sync');
+
+let num1 = prompt("Ingrese el primer valor: ")
+let num2 = prompt("Ingrese el segundo valor: ")
+
+sumar(+num1,+num2);

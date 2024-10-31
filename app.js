6bae7cabbb7bf236e4f2597e7ab82b505ef6b3d5
Julian Aquino <julian.eduardo.aquino@gmail.com>
2024-10-31 20:40:55 -0300

    Se agrego la libreria prompt para interactuar con los usuarios

--- app.js
+++ app.js
@@ -1,12 +1,54 @@
-const sumar = require('./modules/sumar');
-const restar = require('./modules/restar');
+const sumar = require("./modules/sumar");
+const restar = require("./modules/restar");
+const multiplicar = require("./modules/multiplicar");
+const dividir = require("./modules/dividir");
 
-const multiplicar = require('./modules/multiplicar');
-const dividir = require('./modules/dividir');
+const prompt = require("prompt-sync")();
 
-const prompt = require('prompt-sync');
+console.log(`Bienvenido, a la calculadora. Esta aplicacion le permite realizar las siguientes operaciones
+    1* Sumar
+    2* Restar
+    3* Dividir
+    4* Multiplicar`);
 
-let num1 = prompt("Ingrese el primer valor: ")
-let num2 = prompt("Ingrese el segundo valor: ")
+let operacion = prompt("Seleccione alguna de las opciones para operar: ", null);
 
-sumar(+num1,+num2);
+let num1 = prompt("Ingrese el primer valor: ", null);
+let num2 = prompt("Ingrese el primer valor: ", null);
+
+if (
+  operacion.length == 0 ||
+  num1.length == 0 ||
+  num2.length == 0 ||
+  isNaN(+operacion) ||
+  isNaN(+num1) ||
+  isNaN(+num2) 
+) {
+  console.log("No puedo continuar con la operacion");
+} else {
+  operacion = +operacion;
+  num1 = +num1;
+  num2 = +num2;
+
+  switch (operacion) {
+    case 1:
+      console.log(sumar(num1, num2));
+      break;
+    case 2:
+      console.log(restar(num1, num2));
+      break;
+    case 3:
+      console.log(multiplicar(num1, num2));
+      break;
+    case 4:
+      console.log(dividir(num1, num2));
+      break;
+    case 5:
+      console.log(raiz(num1, num2));
+      break;
+    default:
+      console.log(
+        "La operacion indicada no corresponde a las opciones disponibles"
+      );
+  }
+}
--- modules/dividir.js
+++ modules/dividir.js
@@ -1,4 +1,9 @@
 function division(num1,num2){
+    if(num2 == 0){
+        return "La operacion indica no es valida, no se pude dividir por 0"
+    }
+    
     return num1 / num2;
 }
+
 module.exports = division;
